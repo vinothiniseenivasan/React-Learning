be992044de7ced6dd32767944b8d9e61299a3b70
vinothini <vinothiniciet@gmail.com>
2024-06-06 12:38:13 +0400

    React rood des

--- React and ReactDOM/script.js
+++ React and ReactDOM/script.js
@@ -1,5 +1,38 @@
-const heading = React.createElement("h1" , {id: "heading"} ,"Hello World React !");
+ 
+    
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
-    console.log("heading" , heading)
+//     <div id="parent">
+//        <div id="child">
+//         <h1>I am a h1 tag</h1>
+//        </div>
+//   </div>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const parent = React.createElement("div" , {id:"parent"} , 
+                    React.createElement( 
+                         "div" , {id:"child"} , 
+                      [ 
+                        React.createElement("h1" , {} , 
+                            "I am a h1 tag" )  ,
+                         React.createElement("h2" , {} , 
+                            "I am a h2 tag" ) 
+                      ]
+                        )  , 
+
+                        React.createElement(  "div" , {id:"child"} , 
+                      [ 
+                        React.createElement("h3" , {} , 
+                            "I am a h3 tag" )  ,
+                         React.createElement("h4" , {} , 
+                            "I am a h4 tag" ) 
+                      ]
+                        )  
+                    );
+
+root.render(parent);
+console.log("parent" ,parent)
+
+
+
